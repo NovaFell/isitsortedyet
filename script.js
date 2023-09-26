66d0cf75ec11a7e98afbb9a31a128eaddb204f91
Joey <joeyjj.joeyjj@gmail.com>
2023-09-26 12:44:27 -0500

    added comparison security

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function isItSortedYet() {
 	const array = [4,3,5,2,6,8,7,9,1];
 	let isSorted = false
 	
-	while(isSorted == false) {
+	while(isSorted === false) {
 		isSorted = checkIfSorted(array);
 		renderList(array);
 		document.getElementById("array").innerHTML = array;
 	}
-	if(isSorted == true) {
+	if(isSorted === true) {
 		document.getElementById("answer").innerHTML = "The array is sorted! :D";
 	}
 }
